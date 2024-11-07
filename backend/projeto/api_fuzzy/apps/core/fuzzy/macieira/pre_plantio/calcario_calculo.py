b920148965e17a35f0a7038033d37cfcd3a21931
Gabriel Copini <gabriel.desenvolvimento@plasoft.com.br>
2024-11-07 18:05:48 -0300

    ajustes em calagem de macieira

---
+++ backend/projeto/api_fuzzy/apps/core/fuzzy/macieira/pre_plantio/calcario_calculo.py
@@ -0,0 +1,29 @@
+
+
+class Calcario:
+    def fazCalculo(indice_smp) -> float:
+        if(indice_smp >= 6):
+            return 0
+        tabela = {
+            4.4: 29.0, 
+            4.5: 21.0, 
+            4.6: 17.3, 
+            4.7: 15.1, 
+            4.8: 13.3,
+            4.9: 11.9, 
+            5.0: 10.7, 
+            5.1: 9.9, 
+            5.2: 9.1, 
+            5.3: 8.3,
+            5.4: 7.5, 
+            5.5: 6.8, 
+            5.6: 6.1, 
+            5.7: 5.4, 
+            5.8: 4.8,
+            5.9: 4.2, 
+        }
+        
+        # Retorna a quantidade exata ou 0 se o índice não estiver na tabela
+        return tabela.get(indice_smp, 0)
+
+   
--- backend/projeto/api_fuzzy/apps/core/fuzzy/macieira/pre_plantio/controller.py
+++ backend/projeto/api_fuzzy/apps/core/fuzzy/macieira/pre_plantio/controller.py
@@ -1,22 +1,27 @@
 from typing import Tuple
 from .fosforo_fuzzy import FosforoFuzzy
 from .potassio_fuzzy import PotassioFuzzy
+from .calcario_calculo import Calcario
 
 class CalculoFuzzy:
 
     def __init__(self, request):
         self.fosforo = request.data['fosforo']
         self.potassio = request.data['potassio']
-        self.calcario = request.data['calcario']
+        self.indice_smp = request.data['indice_smp']
         self.ctc = request.data['ctc']
         self.argila = request.data['argila']
         self.areaPlantada = float(request.data['areaPlantada'])
 
-    def calcarioCalculo() -> Tuple[str, float, float]:
+    def calcarioCalculo(self) -> Tuple[str, float, float]:
         calcario_tipo: str
         calcario_quantidade_hectare: float
         calcario_quantidade_total: float
 
+        calcario_tipo = 'Calcário Calcítico' #EM MACIEIRAS È MELHOR NAO TER MG NA CALAGEM
+
+        calcario_quantidade_hectare = Calcario.fazCalculo(self.indice_smp) * 1000  #deixar em kg
+        calcario_quantidade_total = calcario_quantidade_hectare * self.areaPlantada / 10000
         #aqui fazer a lógica do calculo chamando as regras
         
         return calcario_tipo, calcario_quantidade_hectare, calcario_quantidade_total
