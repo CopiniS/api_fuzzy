1bf3055446e5d933f2295536d2bfb082e2ba39d7
maviifreitass <mariavitoria.freitas@yahoo.com>
2024-11-09 18:47:36 -0300

    fix: validando CORS para requisições de fora

--- backend/projeto/api_fuzzy/api_fuzzy/settings.py
+++ backend/projeto/api_fuzzy/api_fuzzy/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     #apps do projeto
     'core.apps.CoreConfig',
     'rest_framework',
@@ -55,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'api_fuzzy.urls'
